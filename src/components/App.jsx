e70cb34fba6ea6547af289a9c79f9deb4ea4e0b0
James Hales <jdhales2686@gmail.com>
2023-08-07 20:10:28 -0600

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import Defaultcontainer from "./routing/DefaultContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/" component={Defaultcontainer}></Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
