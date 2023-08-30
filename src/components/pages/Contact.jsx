86828a8adc65b6fffc136c3e368ac1c3b05bc15a
james <jdhales2686@gmail.com>
2023-08-29 18:25:55 -0600

    cleaned up contact page

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -13,22 +13,6 @@ contact
 export default function Contact() {
   return (
     <div className="contact-page">
-      <div className="contact-form-container">
-        <div className="contact-form-wrapper">
-          <div className="contact-field email"></div>
-
-          <div className="contact-field subject"></div>
-
-          <div className="contact-field name"></div>
-
-          <div className="contact-field content"></div>
-        </div>
-
-        <div className="contact-submit">
-          <div>submit button</div>
-        </div>
-      </div>
-
       <div className="contact-information-container">
         <div className="title-wrapper">
           <p>Software Developer</p>
