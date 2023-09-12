38f2416e9b015710123c7c31191f02011318ba1e
james <jdhales2686@gmail.com>
2023-09-12 13:48:14 -0600

    github about isn't invisible, is like project cards with text lighting up when hovered over

--- src/components/pages/Github.jsx
+++ src/components/pages/Github.jsx
@@ -1,10 +1,6 @@
-import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Github() {
-  const [showPersonal, setShowPersonal] = useState(false);
-  const [showDevpipeline, setShowDevpipeline] = useState(false);
-
   return (
     <div className="github-page">
       <div className="eyes">
@@ -22,29 +18,24 @@ export default function Github() {
             href="https://github.com/Jamesh431"
             target="_blank"
             rel="noreferrer"
-            onMouseEnter={() => setShowPersonal(true)}
-            onMouseLeave={() => setShowPersonal(false)}
           >
-            <div className="github-button">
-              <FontAwesomeIcon icon="fa-brands fa-github" size="2xl" />
+            <div className="github-border-wrapper">
+              <div className="github-logo">
+                <FontAwesomeIcon icon="fa-brands fa-github" size="2xl" />
+              </div>
+
+              <div className="github-title">
+                <h2>Personal</h2>
+              </div>
             </div>
 
-            <div className="github-title">
-              <h2>Personal</h2>
+            <div className="github-about">
+              <p>
+                Personally created, eloquently handcrafted, artisanal
+                masterpieces!
+              </p>
             </div>
           </a>
-
-          <div className="github-about">
-            <p
-              style={{
-                transition: "opacity 0.5s ease-in-out",
-                opacity: showPersonal ? 1 : 0,
-              }}
-            >
-              Personally created, eloquently handcrafted, artisanal
-              masterpieces!
-            </p>
-          </div>
         </div>
 
         <div className="github-wrapper devpipeline">
@@ -53,28 +44,21 @@ export default function Github() {
             href="https://github.com/James-Devpipline"
             target="_blank"
             rel="noreferrer"
-            onMouseEnter={() => setShowDevpipeline(true)}
-            onMouseLeave={() => setShowDevpipeline(false)}
           >
-            <div className="github-button">
-              <FontAwesomeIcon icon="fa-brands fa-github" size="2xl" />
+            <div className="github-border-wrapper">
+              <div className="github-logo">
+                <FontAwesomeIcon icon="fa-brands fa-github" size="2xl" />
+              </div>
+
+              <div className="github-title">
+                <h2>Devpipeline</h2>
+              </div>
             </div>
 
-            <div className="github-title">
-              <h2>Devpipeline</h2>
+            <div className="github-about">
+              <p>Devpipeline created this one for my work with devpipeline</p>
             </div>
           </a>
-
-          <div className="github-about">
-            <p
-              style={{
-                transition: "opacity 0.5s ease-in-out",
-                opacity: showDevpipeline ? 1 : 0,
-              }}
-            >
-              Devpipeline created this one for my work with devpipeline
-            </p>
-          </div>
         </div>
       </div>
     </div>
